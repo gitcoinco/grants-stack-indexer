fabf278657787b4d0cfce86d170cfafe0159fc8d
Mohamed Boudra <boudra.moha@gmail.com>
2023-03-31 17:56:24 +0200

    feat: handle metadata and votes async

--- src/handleEvent.ts
+++ src/handleEvent.ts
@@ -10,8 +10,12 @@ import { getPrice } from "./coinGecko.js";
 
 import RoundImplementationABI from "../abis/RoundImplementation.json" assert { type: "json" };
 import QuadraticFundingImplementationABI from "../abis/QuadraticFundingVotingStrategyImplementation.json" assert { type: "json" };
+import { writeFile } from "node:fs";
 
 type Indexer = ChainsauceIndexer<JsonStorage>;
+
+let blocks = 0;
+let events = 0;
 
 async function convertToUSD(
   token: string,
@@ -24,6 +28,7 @@ async function convertToUSD(
   const cacheKey = `price-${token}-${chainId}-${toTimestamp}-${fromTimestamp}`;
 
   const price = await cache.lazy<number>(cacheKey, () => {
+    console.log(cacheKey);
     return getPrice(token, chainId, fromTimestamp, toTimestamp);
   });
 
@@ -57,6 +62,8 @@ function fullProjectId(
 async function handleEvent(indexer: Indexer, event: Event) {
   const db = indexer.storage;
   const chainId = indexer.chainId;
+  events++;
+  console.log("events", events);
 
   switch (event.name) {
     // -- PROJECTS
@@ -76,11 +83,6 @@ async function handleEvent(indexer: Indexer, event: Event) {
     }
 
     case "MetadataUpdated": {
-      const metadata = await cachedIpfs(
-        event.args.metaPtr.pointer,
-        indexer.cache
-      );
-
       const id = fullProjectId(
         indexer.chainId,
         event.args.projectID.toNumber(),
@@ -91,8 +93,23 @@ async function handleEvent(indexer: Indexer, event: Event) {
         await db.collection("projects").updateById(id, (project) => ({
           ...project,
           metaPtr: event.args.metaPtr.pointer,
-          metadata: metadata,
         }));
+
+        return async () => {
+          const metadata = await cachedIpfs(
+            event.args.metaPtr.pointer,
+            indexer.cache
+          );
+
+          // eslint-disable-next-line @typescript-eslint/no-explicit-any
+          await db.collection("projects").updateById(id, (project: any) => {
+            if (project.metaPtr === event.args.metaPtr.pointer) {
+              return { ...project, metadata };
+            }
+
+            return project;
+          });
+        };
       } catch (e) {
         console.error("Project not found", event.args.projectID.toNumber());
       }
@@ -217,129 +234,134 @@ async function handleEvent(indexer: Indexer, event: Event) {
 
     // --- Votes
     case "Voted": {
-      const voteId = ethers.utils.solidityKeccak256(
-        ["string"],
-        [
-          `${event.transactionHash}-${event.args.voter}-${event.args.grantAddress}`,
-        ]
-      );
+      return async () => {
+        const voteId = ethers.utils.solidityKeccak256(
+          ["string"],
+          [
+            `${event.transactionHash}-${event.args.voter}-${event.args.grantAddress}`,
+          ]
+        );
 
-      const projectApplication = await db
-        .collection(`rounds/${event.args.roundAddress}/projects`)
-        .findById(event.args.projectId);
-
-      const round = await db
-        .collection(`rounds`)
-        .findById(event.args.roundAddress);
-
-      if (
-        projectApplication === undefined ||
-        projectApplication.status !== "APPROVED" ||
-        round === undefined
-      ) {
-        // TODO: We seem to be ceceiving votes for projects that have been rejected? Here's an example:
-        // Project ID: 0x79f3e178005bfbe0a3defff8693009bb12e58102763501e52995162820ae3560
-        // Round ID: 0xd95a1969c41112cee9a2c931e849bcef36a16f4c
-        return;
-      }
-
-      const now = new Date();
-
-      const startDate = new Date(round.roundStartTime * 1000);
-      // if round ends in the future, end it now to get live data
-      const endDate = new Date(round.roundEndTime * 1000);
-
-      const amountUSD = await convertToUSD(
-        event.args.token.toLowerCase(),
-        event.args.amount,
-        chainId,
-        Math.floor(startDate.getTime() / 1000),
-        Math.floor(Math.min(now.getTime(), endDate.getTime()) / 1000),
-        indexer.cache
-      );
-
-      const vote = {
-        id: voteId,
-        projectId: event.args.projectId,
-        roundId: event.args.roundAddress,
-        token: event.args.token,
-        voter: event.args.voter,
-        grantAddress: event.args.grantAddress,
-        amount: event.args.amount.toString(),
-        amountUSD: amountUSD,
-      };
-
-      // Insert or update  unique round contributor
-      const roundContributors = db.collection(
-        `rounds/${event.args.roundAddress}/contributors`
-      );
-
-      const existingRoundContributor = await roundContributors.updateById(
-        event.args.voter,
-        (contributor) => ({
-          ...contributor,
-          amountUSD: contributor.amountUSD + amountUSD,
-          votes: contributor.votes + 1,
-        })
-      );
-
-      if (!existingRoundContributor) {
-        await roundContributors.insert({
-          id: event.args.voter,
-          amountUSD,
-          votes: 1,
-        });
-      }
-
-      // Insert or update unique project contributor
-      const projectContributors = db.collection(
-        `rounds/${event.args.roundAddress}/projects/${event.args.projectId}/contributors`
-      );
-
-      const existingProjectContributor = await projectContributors.updateById(
-        event.args.voter,
-        (contributor) => ({
-          ...contributor,
-          amountUSD: contributor.amountUSD + amountUSD,
-          votes: contributor.votes + 1,
-        })
-      );
-
-      if (!existingProjectContributor) {
-        await projectContributors.insert({
-          id: event.args.voter,
-          amountUSD,
-          votes: 1,
-        });
-      }
-
-      await Promise.all([
-        db
-          .collection("rounds")
-          .updateById(event.args.roundAddress, (round) => ({
-            ...round,
-            amountUSD: round.amountUSD + amountUSD,
-            votes: round.votes + 1,
-            uniqueContributors:
-              round.uniqueContributors + (existingRoundContributor ? 0 : 1),
-          })),
-        db
+        const projectApplication = await db
           .collection(`rounds/${event.args.roundAddress}/projects`)
-          .updateById(event.args.projectId, (project) => ({
-            ...project,
-            amountUSD: project.amountUSD + amountUSD,
-            votes: project.votes + 1,
-            uniqueContributors:
-              project.uniqueContributors + (existingProjectContributor ? 0 : 1),
-          })),
-        db.collection(`rounds/${event.args.roundAddress}/votes`).insert(vote),
-        db
-          .collection(
-            `rounds/${event.args.roundAddress}/projects/${event.args.projectId}/votes`
-          )
-          .insert(vote),
-      ]);
-      break;
+          .findById(event.args.projectId);
+
+        const round = await db
+          .collection(`rounds`)
+          .findById(event.args.roundAddress);
+
+        if (
+          projectApplication === undefined ||
+          projectApplication.status !== "APPROVED" ||
+          round === undefined
+        ) {
+          // TODO: We seem to be ceceiving votes for projects that have been rejected? Here's an example:
+          // Project ID: 0x79f3e178005bfbe0a3defff8693009bb12e58102763501e52995162820ae3560
+          // Round ID: 0xd95a1969c41112cee9a2c931e849bcef36a16f4c
+          return;
+        }
+
+        const now = new Date();
+
+        const startDate = new Date(round.roundStartTime * 1000);
+        // if round ends in the future, end it now to get live data
+        const endDate = new Date(round.roundEndTime * 1000);
+
+        const amountUSD = await convertToUSD(
+          event.args.token.toLowerCase(),
+          event.args.amount,
+          chainId,
+          Math.floor(startDate.getTime() / 1000),
+          Math.floor(Math.min(now.getTime(), endDate.getTime()) / 1000),
+          indexer.cache
+        );
+
+        const vote = {
+          id: voteId,
+          projectId: event.args.projectId,
+          roundId: event.args.roundAddress,
+          token: event.args.token,
+          voter: event.args.voter,
+          grantAddress: event.args.grantAddress,
+          amount: event.args.amount.toString(),
+          amountUSD: amountUSD,
+        };
+
+        // Insert or update  unique round contributor
+        const roundContributors = db.collection(
+          `rounds/${event.args.roundAddress}/contributors`
+        );
+
+        const isNewRoundContributor = await roundContributors.upsertById(
+          event.args.voter,
+          (contributor) => {
+            if (contributor) {
+              return {
+                ...contributor,
+                amountUSD: contributor.amountUSD + amountUSD,
+                votes: contributor.votes + 1,
+              };
+            } else {
+              return {
+                id: event.args.voter,
+                amountUSD,
+                votes: 1,
+              };
+            }
+          }
+        );
+
+        // Insert or update unique project contributor
+        const projectContributors = db.collection(
+          `rounds/${event.args.roundAddress}/projects/${event.args.projectId}/contributors`
+        );
+
+        const isNewProjectContributor = await projectContributors.upsertById(
+          event.args.voter,
+          (contributor) => {
+            if (contributor) {
+              return {
+                ...contributor,
+                amountUSD: contributor.amountUSD + amountUSD,
+                votes: contributor.votes + 1,
+              };
+            } else {
+              return {
+                id: event.args.voter,
+                amountUSD,
+                votes: 1,
+              };
+            }
+          }
+        );
+
+        await Promise.all([
+          db
+            .collection("rounds")
+            .updateById(event.args.roundAddress, (round) => ({
+              ...round,
+              amountUSD: round.amountUSD + amountUSD,
+              votes: round.votes + 1,
+              uniqueContributors:
+                round.uniqueContributors + (isNewRoundContributor ? 1 : 0),
+            })),
+          db
+            .collection(`rounds/${event.args.roundAddress}/projects`)
+            .updateById(event.args.projectId, (project) => ({
+              ...project,
+              amountUSD: project.amountUSD + amountUSD,
+              votes: project.votes + 1,
+              uniqueContributors:
+                project.uniqueContributors + (isNewProjectContributor ? 1 : 0),
+            })),
+          db.collection(`rounds/${event.args.roundAddress}/votes`).insert(vote),
+          db
+            .collection(
+              `rounds/${event.args.roundAddress}/projects/${event.args.projectId}/votes`
+            )
+            .insert(vote),
+        ]);
+      };
     }
 
     default:
