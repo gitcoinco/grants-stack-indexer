884318e3aa1028b27f9c4da9a61c7d743dd2326a
Mohamed Boudra <boudra.moha@gmail.com>
2023-04-11 14:09:42 +0200

    feat: update addressess of mainnet and optimism

--- src/config.ts
+++ src/config.ts
@@ -39,15 +39,28 @@ const chains: Chains = {
     subscriptions: [
       {
         address: "0x03506eD3f57892C85DB20C36846e9c808aFe9ef4",
-        abi: "#abis/v1/ProjectRegistry.json",
+        abi: "#abis/v2/ProjectRegistry.json",
       },
       {
         address: "0xE2Bf906f7d10F059cE65769F53fe50D8E0cC7cBe",
         abi: "#abis/v1/RoundFactory.json",
+        events: {
+          RoundCreated: "RoundCreatedV1",
+        },
       },
       {
         address: "0x06A6Cc566c5A88E77B1353Cdc3110C2e6c828e38",
         abi: "#abis/v1/QuadraticFundingVotingStrategyFactory.json",
+      },
+      {
+        address: "0x9Cb7f434aD3250d1656854A9eC7A71EceC6eE1EF",
+        abi: "#abis/v2/RoundFactory.json",
+        fromBlock: 16994474,
+      },
+      {
+        address: "0x4a850F463D1C4842937c5Bc9540dBc803D744c9F",
+        abi: "#abis/v2/QuadraticFundingVotingStrategyFactory.json",
+        fromBlock: 16994526,
       },
     ],
   },
@@ -72,17 +85,17 @@ const chains: Chains = {
       },
     ],
     subscriptions: [
-      // {
-      //   address: "0x832c5391dc7931312CbdBc1046669c9c3A4A28d5",
-      //   abi: "#abis/v1/ProjectRegistry.json",
-      // },
-      // {
-      //   address: "0x5770b7a57BD252FC4bB28c9a70C9572aE6400E48",
-      //   abi: "#abis/v1/RoundFactory.json",
-      //   events: {
-      //     RoundCreated: "RoundCreatedV1",
-      //   },
-      // },
+      {
+        address: "0x832c5391dc7931312CbdBc1046669c9c3A4A28d5",
+        abi: "#abis/v1/ProjectRegistry.json",
+      },
+      {
+        address: "0x5770b7a57BD252FC4bB28c9a70C9572aE6400E48",
+        abi: "#abis/v1/RoundFactory.json",
+        events: {
+          RoundCreated: "RoundCreatedV1",
+        },
+      },
       {
         address: "0xa71864fAd36439C50924359ECfF23Bb185FFDf21",
         abi: "#abis/v2/ProjectRegistry.json",
@@ -123,15 +136,26 @@ const chains: Chains = {
     subscriptions: [
       {
         address: "0x8e1bD5Da87C14dd8e08F7ecc2aBf9D1d558ea174",
-        abi: "#abis/v1/ProjectRegistry.json",
+        abi: "#abis/v2/ProjectRegistry.json",
       },
       {
         address: "0x0f0A4961274A578443089D06AfB9d1fC231A5a4D",
         abi: "#abis/v1/RoundFactory.json",
+        events: {
+          RoundCreated: "RoundCreatedV1",
+        },
       },
       {
         address: "0xE1F4A28299966686c689223Ee7803258Dbde0942",
-        abi: "#abis/v1/QuadraticFundingVotingStrategyFactory.json",
+        abi: "#abis/v2/QuadraticFundingVotingStrategyFactory.json",
+      },
+      {
+        address: "0x04E753cFB8c8D1D7f776f7d7A033740961b6AEC2",
+        abi: "#abis/v2/RoundFactory.json",
+      },
+      {
+        address: "0x838C5e10dcc1e54d62761d994722367BA167AC22",
+        abi: "#abis/v2/QuadraticFundingVotingStrategyFactory.json",
       },
     ],
   },
@@ -163,6 +187,9 @@ const chains: Chains = {
       {
         address: "0x3e7f72DFeDF6ba1BcBFE77A94a752C529Bb4429E",
         abi: "#abis/v1/RoundFactory.json",
+        events: {
+          RoundCreated: "RoundCreatedV1",
+        },
       },
       {
         address: "0x06A6Cc566c5A88E77B1353Cdc3110C2e6c828e38",
