de7f1ce3423a6649271cc9c3ca80e40caf2b1a2a
Mohamed Boudra <boudra.moha@gmail.com>
2023-04-12 16:38:28 +0200

    feat: add event handlers

---
+++ src/handlers/roundMetaPtrUpdated.ts
@@ -0,0 +1,28 @@
+import { Indexer, JsonStorage, Event } from "chainsauce";
+import { fetchJsonCached as ipfs } from "../ipfs.js";
+
+export default async function roundMetaPtrUpdated(
+  { cache, storage: db }: Indexer<JsonStorage>,
+  event: Event
+) {
+  const id = event.address;
+
+  await db.collection("rounds").updateById(id, (round) => ({
+    ...round,
+    metaPtr: event.args.newMetaPtr.pointer,
+  }));
+
+  return async () => {
+    const metaPtr = event.args.newMetaPtr.pointer;
+    const metadata = await ipfs(metaPtr, cache);
+
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    await db.collection("rounds").updateById(id, (round: any) => {
+      if (round.metaPtr === event.args.newMetaPtr.pointer) {
+        return { ...round, metadata };
+      }
+
+      return round;
+    });
+  };
+}
