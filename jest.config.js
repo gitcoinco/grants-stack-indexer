edb1c91d56b5efafeea620b128a46170d995669c
Mohamed Boudra <boudra.moha@gmail.com>
2023-04-26 19:12:08 +0200

    chore: delete jest config

--- jest.config.js
+++
@@ -1,8 +0,0 @@
-/** @type {import('ts-jest').JestConfigWithTsJest} */
-
-export default {
-  preset: "ts-jest/presets/js-with-ts",
-  testEnvironment: "node",
-  resolver: "ts-jest-resolver",
-  transformIgnorePatterns: [`/node_modules/(?!(chainsauce))/`],
-};
