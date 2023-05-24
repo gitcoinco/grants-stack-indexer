fda0ff7bb87938c038a999931b6c0c90a5ae40f2
Mohamed Boudra <boudra.moha@gmail.com>
2023-05-24 11:36:15 +0200

    feat: allow loading a json database without a chainId

--- src/database.ts
+++ src/database.ts
@@ -2,11 +2,16 @@ import config from "./config.js";
 import path from "node:path";
 import { JsonStorage } from "chainsauce";
 
-export default function load(chainId: number): JsonStorage {
-  if (config.chains.find((chain) => chain.id === chainId) === undefined) {
-    throw new Error(`Chain ${chainId} not foound`);
+export default function load(chainId?: number): JsonStorage {
+  let storageDir = config.storageDir;
+
+  if (chainId) {
+    if (config.chains.find((chain) => chain.id === chainId) === undefined) {
+      throw new Error(`Chain ${chainId} not foound`);
+    }
+
+    storageDir = path.join(storageDir, chainId.toString());
   }
 
-  const storageDir = path.join(config.storageDir, chainId.toString());
   return new JsonStorage(storageDir);
 }
