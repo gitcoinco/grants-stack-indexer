e317eb0f056d42eb5d599fd1da1fc4219f08dd2f
Mohamed Boudra <boudra.moha@gmail.com>
2023-06-22 21:09:19 +0200

    feat: add usage to command

--- diff.sh
+++ diff.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
 
+if [ $# -ne 2 ]; then
+  echo "Usage: $0 <old_url> <new_url>"
+  exit 1
+fi
+
 A=`mktemp`
 B=`mktemp`
 DIFF=`mktemp`
+
+trap "rm $A $B $DIFF" EXIT
 
 curl -s $1 | jq . > $A
 curl -s $2 | jq . > $B
@@ -11,5 +18,3 @@ curl -s $2 | jq . > $B
 # use the -f flag to show full diff
 npx json-diff --color $A $B > $DIFF
 less -r --RAW-CONTROL-CHARS $DIFF
-
-rm $A $B $DIFF
