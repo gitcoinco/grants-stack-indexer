9d29d11b3415c3c45ab110d06e17b085f71d32c9
Massimiliano Mirra <max@gitcoin.co>
2023-07-12 17:47:41 +0100

    Foreign chain price retrieval (#148)

--- src/cli/indexer.ts
+++ src/cli/indexer.ts
@@ -6,79 +6,87 @@ import {
   Indexer,
   Event as ChainsauceEvent,
 } from "chainsauce";
-import { fetchJsonCached as ipfs } from "../utils/ipfs.js";
 import fs from "node:fs/promises";
 import path from "node:path";
 
 import "../sentry.js";
 import handleEvent from "../indexer/handleEvent.js";
-import { getIndexerConfig } from "../config.js";
+import { getIndexerConfig, IndexerConfig } from "../config.js";
 import { createPriceProvider, createPriceUpdater } from "../prices/index.js";
 import { importAbi } from "../indexer/utils.js";
+import { fetchJsonCached } from "../utils/ipfs.js";
 
-const config = getIndexerConfig();
-
-if (config.clear) {
-  console.info("Clearing storage directory.");
-  try {
-    await fs.rm(config.storageDir, { recursive: true });
-  } catch {
-    console.info("No storage to clear.");
+async function main(config: IndexerConfig) {
+  if (config.clear) {
+    console.info("Clearing storage directory.");
+    try {
+      await fs.rm(config.storageDir, { recursive: true });
+    } catch {
+      console.info("No storage to clear.");
+    }
   }
-}
 
-const rpcProvider = new RetryProvider({
-  url: config.chain.rpc,
-  timeout: 5 * 60 * 1000,
-});
+  // Dependencies
 
-await rpcProvider.getNetwork();
+  const priceProvider = createPriceProvider(config);
 
-// Update prices to present and optionally keep watching for updates
+  const rpcProvider = new RetryProvider({
+    url: config.chain.rpc,
+    timeout: 5 * 60 * 1000,
+  });
 
-const priceUpdater = createPriceUpdater({
-  rpcProvider,
-  cache: config.cacheDir
-    ? new Cache(path.join(config.cacheDir, "prices"))
-    : null,
-  chain: config.chain,
-});
+  await rpcProvider.getNetwork();
 
-if (config.follow) {
-  await priceUpdater.catchupAndWatch();
-} else {
-  await priceUpdater.fetchPricesUntilBlock(config.toBlock ?? "latest");
-}
+  // Update prices to present and optionally keep watching for updates
 
-// Process blockchain events
-
-const priceProvider = createPriceProvider({});
-
-const indexer = await createIndexer(
-  rpcProvider,
-  new JsonStorage(path.join(config.storageDir, config.chain.id.toString())),
-  (indexer: Indexer<JsonStorage>, event: ChainsauceEvent) => {
-    return handleEvent(event, {
-      db: indexer.storage,
-      indexer,
-      ipfs,
-      priceProvider,
-    });
-  },
-  {
-    toBlock: config.toBlock,
-    logLevel: config.logLevel,
-    eventCacheDirectory: config.cacheDir
-      ? path.join(config.cacheDir, "events")
+  const priceUpdater = createPriceUpdater({
+    ...config,
+    rpcProvider,
+    cache: config.cacheDir
+      ? new Cache(path.join(config.cacheDir, "prices"))
       : null,
-    runOnce: !config.follow,
-  }
-);
+    chain: config.chain,
+  });
 
-for (const subscription of config.chain.subscriptions) {
-  indexer.subscribe(
-    subscription.address,
-    await importAbi(subscription.abi),
-    Math.max(subscription.fromBlock || 0, config.fromBlock)
+  if (config.follow) {
+    await priceUpdater.catchupAndWatch();
+  } else {
+    await priceUpdater.fetchPricesUntilBlock(config.toBlock ?? "latest");
+  }
+
+  // Update blockchain-dependent state to present and optionally keep watching for updates
+
+  const indexer = await createIndexer(
+    rpcProvider,
+    new JsonStorage(path.join(config.storageDir, config.chain.id.toString())),
+    (indexer: Indexer<JsonStorage>, event: ChainsauceEvent) => {
+      return handleEvent(event, {
+        db: indexer.storage,
+        indexer,
+        ipfs: (cid: string) =>
+          fetchJsonCached(cid, indexer.cache, {
+            ipfsGateway: config.ipfsGateway,
+          }),
+        priceProvider,
+      });
+    },
+    {
+      toBlock: config.toBlock,
+      logLevel: config.logLevel,
+      eventCacheDirectory: config.cacheDir
+        ? path.join(config.cacheDir, "events")
+        : null,
+      runOnce: !config.follow,
+    }
   );
+
+  for (const subscription of config.chain.subscriptions) {
+    indexer.subscribe(
+      subscription.address,
+      await importAbi(subscription.abi),
+      Math.max(subscription.fromBlock || 0, config.fromBlock)
+    );
+  }
 }
+
+await main(getIndexerConfig());
--- src/indexer/handlers/roundMetaPtrUpdated.ts
+++ src/indexer/handlers/roundMetaPtrUpdated.ts
@@ -1,16 +1,16 @@
 import { Indexer, JsonStorage } from "chainsauce";
-import { fetchJsonCached as ipfs } from "../../utils/ipfs.js";
 import { Round } from "../types.js";
 import { RoundMetaPtrUpdatedEvent } from "../events.js";
 
 export default async function roundMetaPtrUpdated(
-  { cache, storage: db }: Indexer<JsonStorage>,
-  event: RoundMetaPtrUpdatedEvent
+  { storage: db }: Indexer<JsonStorage>,
+  event: RoundMetaPtrUpdatedEvent,
+  ipfs: <T>(cid: string) => Promise<T | undefined>
 ) {
   const id = event.address;
 
   const metaPtr = event.args.newMetaPtr.pointer;
-  const metadata = await ipfs<Round["metadata"]>(metaPtr, cache);
+  const metadata = await ipfs<Round["metadata"]>(metaPtr);
 
   await db.collection<Round>("rounds").updateById(id, (round) => {
     return {
