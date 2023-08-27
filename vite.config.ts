236cd8bc573d3ce68ce3933d4d94de0877232106
Massimiliano Mirra <max@gitcoin.co>
2023-08-27 17:33:43 +0100

    enh: use leveldb for passport (#266)
    * enh: use leveldb for passport
    
    * enh: throttle progress log
    
    * enh: update chainsauce
    
    * fix: fix tests

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,12 @@ import { configDefaults, defineConfig } from "vitest/config";
 
 export default defineConfig({
   test: {
-    watchExclude: [...configDefaults.watchExclude, "data", "test"],
+    watchExclude: [
+      ...configDefaults.watchExclude,
+      "data",
+      ".cache",
+      "test",
+      "passport_scores.leveldb",
+    ],
   },
 });
