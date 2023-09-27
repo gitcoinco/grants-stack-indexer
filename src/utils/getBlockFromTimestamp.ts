61d81905f882e92c4c038579c1cd9e3f196db827
Mohamed Boudra <boudra.moha@gmail.com>
2023-09-27 14:00:00 +0200

    feat: make block cache optional

--- src/utils/getBlockFromTimestamp.ts
+++ src/utils/getBlockFromTimestamp.ts
@@ -1,67 +1,76 @@
 import { BlockCache } from "../blockCache.js";
 
-export async function getBlockFromTimestamp(
-  chainId: number,
-  timestampInMs: number,
-  startBlock: bigint,
-  endBlock: bigint,
-  getBlockTimestamp: (blockNumber: bigint) => Promise<number>,
-  blockCache: BlockCache
-): Promise<bigint> {
-  console.log("finding block for timestamp", new Date(timestampInMs));
-
+export async function getBlockFromTimestamp({
+  chainId,
+  timestampInSeconds,
+  startBlock,
+  endBlock,
+  getBlockTimestamp,
+  blockCache,
+}: {
+  chainId: number;
+  timestampInSeconds: number;
+  startBlock: bigint;
+  endBlock: bigint;
+  getBlockTimestamp: (blockNumber: bigint) => Promise<number>;
+  blockCache?: BlockCache;
+}): Promise<bigint | null> {
   async function getBlockTimestampCached(blockNumber: bigint) {
-    const block = await blockCache.getBlockByNumber(chainId, blockNumber);
+    if (blockCache) {
+      const block = await blockCache.getBlockByNumber(chainId, blockNumber);
 
-    if (block) {
-      return block.timestamp;
+      if (block) {
+        return block.timestamp;
+      }
+
+      const timestamp = await getBlockTimestamp(blockNumber);
+
+      // console.log("saving");
+      await blockCache.saveBlock({
+        chainId,
+        blockNumber,
+        timestamp,
+      });
+      return timestamp;
     }
 
     const timestamp = await getBlockTimestamp(blockNumber);
 
-    await blockCache.saveBlock({
-      chainId,
-      blockNumber,
-      timestamp,
-    });
-
     return timestamp;
   }
 
-  const targetTimestamp = Math.floor(timestampInMs / 1000);
-  const cachedBlock = await blockCache.getBlockByTimestamp(
-    chainId,
-    targetTimestamp
-  );
+  let low = startBlock;
+  let high = endBlock;
 
-  if (cachedBlock) {
-    return cachedBlock.blockNumber;
+  if (blockCache) {
+    const { before: lowerBound, after: upperBound } =
+      await blockCache.getClosestBoundsForTimestamp(
+        chainId,
+        timestampInSeconds
+      );
+
+    if (lowerBound) {
+      low = lowerBound.blockNumber;
+    }
+
+    if (upperBound) {
+      high = upperBound.blockNumber;
+    }
   }
 
-  const { before: lowerBound, after: upperBound } =
-    await blockCache.getNearestBlocksByTimestamp(chainId, targetTimestamp);
-
-  let low = lowerBound ? lowerBound.blockNumber : startBlock;
-  let high = upperBound ? upperBound.blockNumber : endBlock;
-
-  console.log("finding between", targetTimestamp, lowerBound, upperBound);
+  let result = null;
 
   while (low <= high) {
     const mid = (low + high) / BigInt(2);
-
-    if (low === high) {
-      console.error("-------------------> low === high");
-      return low;
-    }
-
     const midTimestamp = await getBlockTimestampCached(mid);
 
-    if (midTimestamp < targetTimestamp) {
-      low = mid + BigInt(1);
-    } else if (midTimestamp > targetTimestamp) {
+    if (midTimestamp >= timestampInSeconds) {
+      result = mid;
       high = mid - BigInt(1);
+    } else {
+      low = mid + BigInt(1);
     }
   }
 
-  return low;
+  return result;
 }
