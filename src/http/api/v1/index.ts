163ffe9e475bc39ec4d21187fa617788705d9798
Mohamed Boudra <boudra.moha@gmail.com>
2023-11-29 13:04:38 +0100

    chore: remove console log

--- src/http/api/v1/index.ts
+++ src/http/api/v1/index.ts
@@ -25,7 +25,6 @@ export const createHandler = (config: HttpApiConfig): express.Router => {
         return;
       }
 
-      console.error(err);
       config.logger.error({ msg: "Unexpected exception", err });
 
       Sentry.captureException(err);
