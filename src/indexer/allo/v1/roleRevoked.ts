191e6fee0920f9864ca2cf9c421f232723a780c2
Mohamed Boudra <boudra.moha@gmail.com>
2024-01-30 11:33:43 +0100

    Revert "Revert "index roleGranted and roleRevoked for Allo V1 rounds""

--- src/indexer/allo/v1/roleRevoked.ts
+++ src/indexer/allo/v1/roleRevoked.ts
@@ -2,7 +2,12 @@ import { EventHandlerArgs } from "chainsauce";
 import type { Indexer } from "../../indexer.js";
 import { Changeset } from "../../../database/index.js";
 import { parseAddress } from "../../../address.js";
-import { PROGRAM_ADMIN_ROLE, PROGRAM_OPERATOR_ROLE } from "./roles.js";
+import {
+  PROGRAM_ADMIN_ROLE,
+  PROGRAM_OPERATOR_ROLE,
+  ROUND_ADMIN_ROLE,
+  ROUND_OPERATOR_ROLE,
+} from "./roles.js";
 
 export default async function handleEvent(
   args: EventHandlerArgs<
@@ -20,7 +25,7 @@ export default async function handleEvent(
   switch (args.event.contractName) {
     case "AlloV1/ProgramImplementation/V1": {
       const programAddress = parseAddress(event.address);
-      const project = await db.getProjectById(programAddress);
+      const project = await db.getProjectById(chainId, programAddress);
       if (project === null) {
         logger.error({
           msg: `Program/Project ${programAddress} not found`,
@@ -64,6 +69,60 @@ export default async function handleEvent(
         default: {
           logger.error({
             msg: `Unknown role ${role} for program ${programAddress}`,
+            event,
+          });
+          return [];
+        }
+      }
+    }
+
+    case "AlloV1/RoundImplementation/V1":
+    case "AlloV1/RoundImplementation/V2": {
+      const roundAddress = parseAddress(event.address);
+      const round = await db.getRoundById(chainId, roundAddress);
+      if (round === null) {
+        logger.error({
+          msg: `Round ${roundAddress} not found`,
+          event,
+        });
+        return [];
+      }
+
+      const account = parseAddress(event.params.account);
+      const role = event.params.role.toLocaleLowerCase();
+
+      switch (role) {
+        case ROUND_ADMIN_ROLE: {
+          return [
+            {
+              type: "DeleteAllRoundRolesByRoleAndAddress",
+              roundRole: {
+                chainId,
+                roundId: roundAddress,
+                role: "admin",
+                address: account,
+              },
+            },
+          ];
+        }
+
+        case ROUND_OPERATOR_ROLE: {
+          return [
+            {
+              type: "DeleteAllRoundRolesByRoleAndAddress",
+              roundRole: {
+                chainId,
+                roundId: roundAddress,
+                role: "manager",
+                address: account,
+              },
+            },
+          ];
+        }
+
+        default: {
+          logger.error({
+            msg: `Unknown role ${role} for program ${roundAddress}`,
             event,
           });
           return [];
