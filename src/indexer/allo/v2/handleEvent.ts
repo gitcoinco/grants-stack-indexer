f82d12caa8fdf9b0ee8e6df634fdf997547500c4
0xKurt <Kurt@Merbeth.io>
2024-02-13 11:12:34 +0100

    fix build

--- src/indexer/allo/v2/handleEvent.ts
+++ src/indexer/allo/v2/handleEvent.ts
@@ -56,7 +56,7 @@ export async function handleEvent(
             id: profileId,
             name: event.params.name,
             nonce: event.params.nonce,
-            anchor: event.params.anchor,
+            anchor: parseAddress(event.params.anchor),
             projectNumber: null,
             metadataCid: metadataCid,
             metadata: metadata,
