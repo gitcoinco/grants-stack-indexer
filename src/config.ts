cf1af9c5e5d13b6946f53ca284e0728bb3f7f3c9
eagle <aparokshambhargav@gmail.com>
2024-02-20 16:37:32 +0530

    fix: update scroll config

--- src/config.ts
+++ src/config.ts
@@ -1211,12 +1211,12 @@ const CHAINS: Chain[] = [
         },
       },
       {
-        code: "MTK",
-        address: "0xc2332031de487f430fae3290c05465d907785eda",
-        decimals: 18,
+        code: "USDC",
+        address: "0x06eFdBFf2a14a7c8E15944D1F4A48F9F95F663A4",
+        decimals: 6,
         priceSource: {
           chainId: 1,
-          address: "0x6B175474E89094C44Da98b954EedeAC495271d0F",
+          address: "0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48",
         },
       },
     ],
