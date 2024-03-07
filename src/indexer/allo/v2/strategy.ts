bec447da213af291c0ec1030100d28e093ca782d
0xKurt <Kurt@Merbeth.io>
2024-03-07 11:59:36 +0100

    update strategy id

--- src/indexer/allo/v2/strategy.ts
+++ src/indexer/allo/v2/strategy.ts
@@ -68,6 +68,8 @@ export function extractStrategyFromId(_id: string): Strategy | null {
     case "0x6f9291df02b2664139cec5703c124e4ebce32879c74b6297faa1468aa5ff9ebf":
     // DonationVotingMerkleDistributionDirectTransferStrategyv1.1
     case "0x2f46bf157821dc41daa51479e94783bb0c8699eac63bf75ec450508ab03867ce":
+    // DonationVotingMerkleDistributionDirectTransferStrategyv2.0
+    case "0x2f0250d534b2d59b8b5cfa5eb0d0848a59ccbf5de2eaf72d2ba4bfe73dce7c6b":
       return {
         id: id,
         name: "allov2.DonationVotingMerkleDistributionDirectTransferStrategy",
