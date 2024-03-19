4a1269f93c39829d75e6d12666286cb19ab87442
0xKurt <Kurt@Merbeth.io>
2024-03-19 10:20:30 +0100

    fix version

--- src/indexer/allo/v2/strategy.ts
+++ src/indexer/allo/v2/strategy.ts
@@ -70,7 +70,7 @@ export function extractStrategyFromId(_id: string): Strategy | null {
     case "0x2f46bf157821dc41daa51479e94783bb0c8699eac63bf75ec450508ab03867ce":
     // DonationVotingMerkleDistributionDirectTransferStrategyv2.0
     case "0x2f0250d534b2d59b8b5cfa5eb0d0848a59ccbf5de2eaf72d2ba4bfe73dce7c6b":
-    // DonationVotingMerkleDistributionDirectTransferStrategyv2.0
+    // DonationVotingMerkleDistributionDirectTransferStrategyv2.1
     case "0x9fa6890423649187b1f0e8bf4265f0305ce99523c3d11aa36b35a54617bb0ec0":
       return {
         id: id,
