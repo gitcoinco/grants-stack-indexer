0d0661a1a0be76e1411b652112f5ef06f5ff88c3
Kurt <kurt@merbeth.io>
2024-03-20 12:48:08 +0100

    fix usdc sepolia (#501)

--- src/config.ts
+++ src/config.ts
@@ -252,6 +252,15 @@ const CHAINS: Chain[] = [
           address: "0x0000000000000000000000000000000000000000",
         },
       },
+      {
+        code: "USDC",
+        address: "0x78e0D07C4A08adFfe610113310163b40E7e47e81",
+        decimals: 6,
+        priceSource: {
+          chainId: 1,
+          address: "0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48",
+        },
+      },
     ],
     subscriptions: [
       {
@@ -397,15 +406,6 @@ const CHAINS: Chain[] = [
         priceSource: {
           chainId: 1,
           address: "0x0000000000000000000000000000000000000000",
-        },
-      },
-      {
-        code: "USDC",
-        address: "0x78e0D07C4A08adFfe610113310163b40E7e47e81",
-        decimals: 6,
-        priceSource: {
-          chainId: 1,
-          address: "0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48",
         },
       },
       {
