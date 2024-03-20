47828a4f3eab83ffa64a543c08a7bf253b50b35f
Aditya Anand M C <aditya.anandmc@gmail.com>
2024-03-21 05:18:08 +0530

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -10,7 +10,7 @@ import os from "node:os";
 type ChainId = number;
 type CoingeckoSupportedChainId = 1 | 10 | 250 | 42161 | 43114;
 
-const CHAIN_DATA_VERSION = "52";
+const CHAIN_DATA_VERSION = "53";
 
 export type Token = {
   code: string;
