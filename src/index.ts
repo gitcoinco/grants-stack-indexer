71c911916ec75f1ff9c7a56be60555fbffe501e9
gnomadic <edflem+github@gmail.com>
2024-08-08 14:05:15 -0400

    stashing

--- src/index.ts
+++ src/index.ts
@@ -25,6 +25,7 @@ import { createPassportProvider, PassportProvider } from "./passport/index.js";
 
 import { createResourceMonitor } from "./resourceMonitor.js";
 import diskstats from "diskstats";
+import { StatsD } from "hot-shots"
 
 import { Chain, getConfig, Config, getChainConfigById } from "./config.js";
 import { createPriceProvider, PriceProvider } from "./prices/provider.js";
@@ -45,6 +46,9 @@ import { createPublicClient, http } from "viem";
 import { IndexerEvents } from "chainsauce/dist/indexer.js";
 
 const RESOURCE_MONITOR_INTERVAL_MS = 1 * 60 * 1000; // every minute
+
+const dogstatsd = new StatsD();
+
 
 function createPgPool(args: { url: string; logger: Logger }): pg.Pool {
   const pool = new Pool({
@@ -72,7 +76,12 @@ function createPgPool(args: { url: string; logger: Logger }): pg.Pool {
   return pool;
 }
 
+
+
 async function main(): Promise<void> {
+
+
+
   const config = getConfig();
 
   // https://github.com/gitcoinco/allo-indexer/issues/215#issuecomment-1711380810
@@ -392,6 +401,21 @@ async function main(): Promise<void> {
               },
       },
     });
+
+
+    dogstatsd.increment('indexer.main', 1);
+
+
+
+
+
+    // tracer.dogstatsd.increment('indexer.main', 1);
+    // tracer.dogstatsd.flush();
+
+
+
+    // baseLogger.info("just flushed datadog");
+
 
     await httpApi.start();
   }
