dd649de1b1e9e1406862b6723726fc03a344676c
0xKurt <kurt@merbeth.io>
2025-03-11 11:52:31 +0100

    added test GTC

--- src/config.ts
+++ src/config.ts
@@ -320,6 +320,15 @@ const CHAINS: Chain[] = [
           address: "0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48",
         },
       },
+      {
+        code: "GTC",
+        address: "0x5e7C95EaF08D6FeD05a8E4BC607Fb682834C74cE",
+        decimals: 18,
+        priceSource: {
+          chainId: 1,
+          address: "0xde30da39c46104798bb5aa3fe8b9e0e1f348163f",
+        },
+      },
     ],
     subscriptions: [
       {
