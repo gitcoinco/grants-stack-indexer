c175d60c23ee2bd548fee67ef5820ea9e99c25d7
Huss Martinez <husse.dev@gmail.com>
2025-03-14 10:45:17 +0700

    removed logstat

--- src/index.ts
+++ src/index.ts
@@ -48,12 +48,6 @@ import { IndexerEvents } from "chainsauce/dist/indexer.js";
 
 const RESOURCE_MONITOR_INTERVAL_MS = 1 * 60 * 1000; // every minute
 
-const dogstatsd = new StatsD({
-  host: "149.248.217.210",
-  port: 8125,
-  tagPrefix: "allo-indexer.",
-});
-
 function createPgPool(args: { url: string; logger: Logger }): pg.Pool {
   const pool = new Pool({
     connectionString: args.url,
@@ -701,15 +695,6 @@ async function catchupAndWatchChain(
           });
 
           const donationQueueLength = db.donationQueueLength();
-
-          dogstatsd.gauge(
-            `indexer.progress.${config.chain.id}`,
-            Number(progressPercentage)
-          );
-          dogstatsd.gauge(
-            `indexer.blockhead.${config.chain.id}`,
-            Number(targetBlock)
-          );
 
           indexerLogger.info(
             `${currentBlock}/${targetBlock} indexed (${progressPercentage}%) (pending events: ${pendingEventsCount}) (pending donations: ${donationQueueLength}) (contracts: ${activeSubscriptions.length})`
